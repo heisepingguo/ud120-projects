df2ed507fcead4c5405baa4faa64e514e02f3bd8
DarkHorse <ljx920612@163.com>
2018-03-11 22:29:22 +0800

    Add time() function to compare the time cost between training and prediction.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -28,8 +28,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
 pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
 check_pred = pred == labels_test
 accurate_counts = check_pred.sum()
 total_nums = len(pred)
