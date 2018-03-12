05587816e212ce581ac33c4a666c850b4e1212d5
DarkHorse <ljx920612@163.com>
2018-03-12 21:40:47 +0800

    Tuned C and kernel parameters for SVM algorithm and check the predictions of test set.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,17 +24,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-from sklearn.svm import SVC
-clf = SVC(kernel = "linear")
-t0 = time()
-clf.fit(features_train, labels_train)
-print "training_time", round(time()-t0, 3), "s"
-t0 = time()
-pred = clf.predict(features_test)
-print "prediction_time", round(time()-t0, 3), "s"
-from sklearn.metrics import accuracy_score
-acc = accuracy_score(pred, labels_test)
-print acc
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+def svm_classifier(C=1.0):
+	from sklearn.svm import SVC
+	clf = SVC(C=C, kernel = "rbf")
+	t0 = time()
+	clf.fit(features_train, labels_train)
+	print "training_time", round(time()-t0, 3), "s"
+	t0 = time()
+	pred = clf.predict(features_test)
+	print "prediction_time", round(time()-t0, 3), "s"
+	from sklearn.metrics import accuracy_score
+	acc = accuracy_score(pred, labels_test)
+	print "For C = "+str(C)+": accuracy is "+str(acc)
+	return pred
+
+#for C in [1.0,10.0,100.0,1000.0,10000.0]:
+pred = svm_classifier(C=10000.0)
+print "There are "+str(sum(pred))+"events "+"predicted to be in the Chris(1) class."
+#answer=[pred[10],pred[26],pred[50]]
+#print answer
 #########################################################
 
 
